552bf143589f46351b7117ee18100741447ac460
Audrius Rudalevicius <d3trax@gmail.com>
2018-01-31 14:28:16 +0200

    add PyQt5 requirement

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ DATA_FILES = [
 ]
 
 INSTALL_REQUIRES = [
-    'Orange3', 'catboost>=0.5.2.1', 'numpy', 'pandas', 'dedupe', 'AnyQt'
+    'Orange3', 'catboost>=0.5.2.1', 'numpy', 'pandas', 'dedupe', 'AnyQt', 'PyQt5'
 ]
 
 ENTRY_POINTS = {
